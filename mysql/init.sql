27701cf40639a5d797bd3053139e94d4d420238f
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2023-06-21 17:02:40 +0200

    expose eduPersonTargetedID
    mostly following:
    https://web.archive.org/web/20170213144759/https://www.eduid.cz/cs/tech/idp/shibboleth/persistentid

---
+++ mysql/init.sql
@@ -0,0 +1,19 @@
+SET NAMES 'utf8';
+SET CHARACTER SET utf8;
+CHARSET utf8;
+CREATE DATABASE IF NOT EXISTS shibboleth CHARACTER SET=utf8;
+USE shibboleth;
+CREATE TABLE IF NOT EXISTS `shibpid` (
+          `localEntity` VARCHAR(255) NOT NULL,
+          `peerEntity` VARCHAR(255) NOT NULL,
+          `principalName` VARCHAR(255) NOT NULL DEFAULT '',
+          `localId` VARCHAR(255) NOT NULL,
+          `persistentId` VARCHAR(50) NOT NULL,
+          `peerProvidedId` VARCHAR(255) DEFAULT NULL,
+          `creationDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          `deactivationDate` TIMESTAMP NULL DEFAULT NULL,
+          PRIMARY KEY (localEntity, peerEntity, persistentId)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+GRANT ALL PRIVILEGES ON shibboleth.* TO 'shibboleth'@`%` IDENTIFIED BY 'silne_heslo';
+FLUSH PRIVILEGES;
